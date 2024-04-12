62d034e83d8a2fa20809dad069c9b8ee45790550
omar2300 <omaralhassan90@gmail.com>
2024-04-12 05:42:22 +0200

    reworked data and trivia ½ done

--- script.js
+++ script.js
@@ -6,218 +6,364 @@ const O_MARKER = 'O';
 let currentPlayer = X_MARKER;
 let board = ['', '', '', '', '', '', '', '', ''];
 
-const allRegions = ['Ionia', 'Demacia', 'Piltover', 'Noxus', 'Void'];
+const allRegions = ['Ionia', 'Demacia', 'Piltover', 'Noxus', 'Void','Darken','Zaun','Bilgewater','Freljord','ShadowIsles','Shurima','Ixtal','Targon','BandleCity','?'];
 const allLanes = ['Toplane', 'Midlane', 'Botlane', 'Support', 'Jungle'];
+// Champion data with regions and lanes
+const championData = {
+  //Toplane
+  "Atrox": [
+    { region: "Darken", lane: "Toplane" }
+  ],
+  "Akali": [
+    { region: "Ionia", lane: "Toplane" },
+    { region: "Ionia", lane: "Midlane" }
+  ],
+  "Camille": [
+    { region: "Piltover", lane: "Toplane" }
+  ],
+  "Cho'Gath": [
+    { region: "Void", lane: "Toplane" }
+  ],
+  "Darius": [
+    { region: "Noxus", lane: "Toplane" }
+  ],
+  "Dr. Mundo": [
+    { region: "Zaun", lane: "Toplane" }
+  ],
+  "Fiora": [
+    { region: "Demacia", lane: "Toplane" }
+  ],
+  "Gangkplank": [
+    { region: "Bilgewater", lane: "Toplane" }
+  ],
+  "Garen": [
+    { region: "Demacia", lane: "Toplane" }
+  ],
+  "Gnar": [
+    { region: "Freljord", lane: "Toplane" }
+  ],
+  "Gragas": [
+    { region: "Freljord", lane: "Toplane" },
+    { region: "Freljord", lane: "Jungle" }
+  ],
+  "Gwen": [
+    { region: "ShadowIsles", lane: "Toplane" }
+  ],
+  "Heimerdinger": [
+    { region: "Piltover", lane: "Toplane" }
+  ],
+  "Illaoi": [
+    { region: "Bilgewater", lane: "Toplane" }
+  ],
+  "Irelia": [
+    { region: "Ionia", lane: "Midlane" },
+    { region: "Ionia", lane: "Toplane" }
+  ],
+  "Jax": [
+    { region: "Shurima", lane: "Toplane" }
+  ],
+  "Jayce": [
+    { region: "Piltover", lane: "Toplane" },
+    { region: "BandleCity", lane: "Midlane" }   
+  ],
+  //Todo: K'Sante not working not knowing why 
+  "K'Sante": [
+    { region: "Shurima", lane: "Toplane" }
+  ],
+  "Kayle": [
+    { region: "Demacia", lane: "Toplane" }
+  ],
+  "Kennen": [
+    { region: "Ionia", lane: "Toplane" }
+  ],
+  "Kennen": [
+    { region: "Ionia", lane: "Toplane" }
+  ],
+  "Kled": [
+    { region: "Noxus", lane: "Toplane" }
+  ],
+  "Malphite": [
+    { region: "Ixtal", lane: "Toplane" }
+  ],
+  "Mordekaiser": [
+    { region: "Noxus", lane: "Toplane" }
+  ],
+  "Nasus": [
+    { region: "Shurima", lane: "Toplane" }
+  ],
+  "Olaf": [
+    { region: "Freljord", lane: "Toplane" }
+  ],
+  "Ornn": [
+    { region: "Freljord", lane: "Toplane" }
+  ],
+  "Pantheon": [
+    { region: "Targon", lane: "Toplane" }
+  ],
+  "Quinn": [
+    { region: "Demacia", lane: "Toplane" }
+  ],
+  "Rek'Sai": [
+    { region: "void", lane: "Toplane" },
+    { region: "void", lane: "Jungle"}
+  ],
+  "Renekton": [
+    { region: "Shurima", lane: "Toplane" }
+  ],
+  "Riven": [
+    { region: "Noxus", lane: "Toplane" }
+  ],
+  "Rumble": [
+    { region: "BandleCity", lane: "Toplane" }
+  ],
+  "Sett": [
+    { region: "Ionia", lane: "Toplane" }
+  ],
+  "Shen": [
+    { region: "Ionia", lane: "Toplane" }
+  ],
+  "Shen": [
+    { region: "Ionia", lane: "Toplane" }
+  ],
+  "Singed": [
+    { region: "Zaun", lane: "Toplane" }
+  ],
+   "Sion": [
+    { region: "Noxus", lane: "Toplane" }
+  ],
+  "Tahm Kench": [
+    { region: "Bilgewater", lane: "Toplane" }
+  ],
+  "Teemo": [
+    { region: "BandleCity", lane: "Toplane" }
+  ],
+  "Trundle": [
+    { region: "Freljord", lane: "Toplane" }
+  ],
+  "Tryndamere": [
+    { region: "Freljord", lane: "Toplane" }
+  ],
+  "Udyr": [
+    { region: "Freljord", lane: "Toplane" },
+    { region: "Freljord", lane: "Jungle" }
+  ],
+  "Urgot": [
+    { region: "Zaun", lane: "Toplane" }
+  ],
+  "Vayne": [
+    { region: "Demacia", lane: "Toplane" }
+  ],
+  "Volibear": [
+    { region: "Freljord", lane: "Toplane" }
+  ],
+  "Yasuo": [
+    { region: "Ionia", lane: "Toplane" }
+  ],
+  "Yone": [
+    { region: "Ionia", lane: "Toplane" }
+  ],
+  "Yorick": [
+    { region: "ShadowIsles", lane: "Toplane" }
+  ],
+  "Zac": [
+    { region: "Zaun", lane: "Toplane" },
+      { region: "Zaun", lane: "Jungle" }
+  ],
+//Jungle
+"Amumu": [
+  { region: "Shurima", lane: "Jungle" }
+],
+"Bel'Veth": [
+  { region: "Void", lane: "Jungle" }
+],
+"Brand": [
+  { region: "Freljord", lane: "Jungle" }
+],
+"Briar": [
+  { region: "Noxus", lane: "Jungle" }
+],
+"Diana": [
+  { region: "Targon", lane: "Jungle" },
+  { region: "Targon", lane: "Midlane" }
+],
+"Ekko": [
+  { region: "Zaun", lane: "Jungle" },
+  { region: "Zaun", lane: "Midlane" }
+],
+"Elise": [
+  { region: "ShadowIsles", lane: "Jungle" }
+],
+"Evelynn": [
+  { region: "?", lane: "Jungle" }
+],
+//not working 
+"Fiddlesticks": [
+  { region: "?", lane: "Jungle" }
+],
+"Graves": [
+  { region: "Bilgewater", lane: "Jungle" }
+],
+"Hecarim": [
+  { region: "Bilgewater", lane: "Jungle" }
+],
+"Ivern": [
+  { region: "Ionia", lane: "Jungle" }
+],
+"Jarvan IV": [
+  { region: "Demacia", lane: "Jungle" }
+],
+"Karthus": [
+  { region: "ShadowIsles", lane: "Jungle" }
+],
+"Kayn": [
+  { region: "Ionia", lane: "Jungle" }
+],
+"Rhaast": [
+  { region: "Darken", lane: "Jungle" }
+],
+"Kha'Zix": [
+  { region: "Void", lane: "Jungle" }
+],
+"Kindred": [
+  { region: "?", lane: "Jungle" }
+],
+"Lee Sin": [
+  { region: "Ionia", lane: "Jungle" }
+],
+"Lillia": [
+  { region: "Ionia", lane: "Jungle" }
+],
+"Lee Sin": [
+  { region: "Ionia", lane: "Jungle" }
+],
+"Master Yi": [
+  { region: "Ionia", lane: "Jungle" }
+],
+"Nidalee": [
+  { region: "Ionia", lane: "Jungle" }
+],
+"Nocturne": [
+  { region: "?", lane: "Jungle" }
+],
+"Nunu & Wilump": [
+  { region: "Freljord", lane: "Jungle" }
+],
+"Poppy": [
+  { region: "Demacia", lane: "Jungle" }
+],
+"Rammus": [
+  { region: "Shurima", lane: "Jungle" }
+],
+"Renger": [
+  { region: "Ixtal", lane: "Jungle" }
+],
+"Sejuani": [
+  { region: "Freljord", lane: "Jungle" }
+],
+"Shaco": [
+  { region: "?", lane: "Jungle" }
+],
+"Shyvana": [
+  { region: "Demacia", lane: "Jungle" }
+],
+"Skarner": [
+  { region: "Ixtal", lane: "Jungle" }
+],
+"Taliyah": [
+  { region: "Shurima", lane: "Jungle" }
+],
+"Vi": [
+  { region: "Piltover", lane: "Jungle" }
+],
+"Viego": [
+  { region: "ShadowIsles", lane: "Jungle" }
+],
+"Warwick": [
+  { region: "Zaun", lane: "Jungle" }
+],
+"Wukong": [
+  { region: "Ionia", lane: "Jungle" }
+],
+"Xin Zhao": [
+  { region: "?", lane: "Jungle" }
+],
+//Midlane 
+"Ahri": [
+  { region: "Ionia", lane: "Midlane" }
+],
+"Akshan": [
+  { region: "Shurima", lane: "Midlane" }
+],
+"Anivia": [
+  { region: "Freljord", lane: "Midlane" }
+],
+"Annie": [
+  { region: "?", lane: "Midlane" }
+],
+"Aurelian Sol": [
+  { region: "Targon", lane: "Midlane" }
+],
+"Azir": [
+  { region: "Shurima", lane: "Midlane" }
+],
+"Cassiopeia": [
+  { region: "Noxus", lane: "Midlane" }
+],
+//not working
+"Corki": [
+  { region: "BandleCity", lane: "Midlane" }
+],
+"Fizz": [
+  { region: "Bilgewater", lane: "Midlane" }
+],
+"Galio": [
+  { region: "Demacia", lane: "Midlane" }
+],
+"Hwei": [
+  { region: "Ionia", lane: "Midlane" }
+],
+"Karma": [
+  { region: "Ionia", lane: "Midlane" }
+],
+"Kassadin": [
+  { region: "Void", lane: "Midlane" }
+],
 
-// League of Legends trivia questions
-const trivia = [
-  {
-    region: 'Ionia',
-    lane: 'Toplane',
-    question:
-      'Which champion is associated with the region of Ionia and the role of Toplane?',
-    answer: 'Kennen',
-  },
-  {
-    region: 'Ionia',
-    lane: 'Midlane',
-    question:
-      'Which champion is associated with the region of Ionia and the role of Midlane?',
-    answer: 'Irelia',
-  },
-  {
-    region: 'Ionia',
-    lane: 'Toplane',
-    question:
-      'Which champion is associated with the region of Ionia and the role of Toplane?',
-    answer: 'Irelia',
-  },
-  {
-    region: 'Ionia',
-    lane: 'Botlane',
-    question:
-      'Which champion is associated with the region of Ionia and the role of Botlane?',
-    answer: 'Jhin',
-  },
-  {
-    region: 'Demacia',
-    lane: 'Toplane',
-    question:
-      'Which champion is associated with the region of Demacia and the role of Toplane?',
-    answer: 'Garen',
-  },
-  {
-    region: 'Demacia',
-    lane: 'Midlane',
-    question:
-      'Which champion is associated with the region of Demacia and the role of Midlane?',
-    answer: 'Lux',
-  },
-  {
-    region: 'Demacia',
-    lane: 'Support',
-    question:
-      'Which champion is associated with the region of Demacia and the role of Support?',
-    answer: 'Lux',
-  },
-  {
-    region: 'Demacia',
-    lane: 'Botlane',
-    question:
-      'Which champion is associated with the region of Demacia and the role of Botlane?',
-    answer: 'Vayne',
-  },
-  
-  {
-    region: 'Ionia',
-    lane: 'Support',
-    question:
-      'Which champion is associated with the region of Ionia and the role of Support?',
-    answer: 'Karma',
-  },
-  {
-    region: 'Ionia',
-    lane: 'Jungle',
-    question:
-      'Which champion is associated with the region of Ionia and the role of Jungle?',
-    answer: 'Master Yi',
-  },
-  {
-    region: 'Demacia',
-    lane: 'Jungle',
-    question:
-      'Which champion is associated with the region of Demacia and the role of Jungle?',
-    answer: 'Jarvan IV',
-  },
-  {
-    region: 'Piltover',
-    lane: 'Jungle',
-    question:
-      'Which champion is associated with the region of Piltover and the role of Jungle?',
-    answer: 'Vi',
-  },
-  {
-    region: 'Noxus',
-    lane: 'Support',
-    question:
-      'Which champion is associated with the region of Noxus and the role of Support?',
-    answer: 'Swain',
-  },
-  {
-    region: 'Noxus',
-    lane: 'Midlane',
-    question:
-      'Which champion is associated with the region of Noxus and the role of Midlane?',
-    answer: 'Swain',
-  },
-  {
-    region: 'Void',
-    lane: 'Support',
-    question:
-      'Which champion is associated with the region of Void and the role of Support?',
-    answer: "Vel'Koz",
-  },
-  {
-    region: 'Void',
-    lane: 'Midlane',
-    question:
-      'Which champion is associated with the region of Void and the role of Midlane?',
-    answer: "Vel'Koz",
-  },
-  {
-    region: 'Void',
-    lane: 'Jungle',
-    question:
-      'Which champion is associated with the region of Void and the role of Jungle?',
-    answer: "Rek'Sai",
-  },
-  {
-    region: 'Piltover',
-    lane: 'Toplane',
-    question:
-      'Which champion is associated with the region of Piltover and the role of Toplane?',
-    answer: 'camille',
-  },
-  {
-    region: 'Void',
-    lane: 'Toplane',
-    question:
-      'Which champion is associated with the region of Void and the role of Toplane?',
-    answer: "Cho'Gath",
-  },
-  {
-    region: 'Void',
-    lane: 'Midlane',
-    question:
-      'Which champion is associated with the region of Void and the role of Midlane?',
-    answer: 'Malzahar',
-  },
-  {
-    region: 'Void',
-    lane: 'Botlane',
-    question:
-      'Which champion is associated with the region of Void and the role of Botlane?',
-    answer: "Kai'Sa",
-  },
-  {
-    region: 'Void',
-    lane: 'Support',
-    question:
-      'Which champion is associated with the region of Void and the role of Support?',
-    answer: "Vel'Koz",
-  },
-  {
-    region: 'Noxus',
-    lane: 'Jungle',
-    question:
-      'Which champion is associated with the region of Noxus and the role of Jungle?',
-    answer: 'Katarina',
-  },
-  {
-    region: 'Void',
-    lane: 'Jungle',
-    question:
-      'Which champion is associated with the region of Void and the role of Jungle?',
-    answer: "Rek'Sai",
-  },
-  {
-    region: 'Piltover',
-    lane: 'Toplane',
-    question:
-      'Which champion is associated with the region of Piltover and the role of Toplane?',
-    answer: 'Jayce',
-  },
-  {
-    region: 'Piltover',
-    lane: 'Midlane',
-    question:
-      'Which champion is associated with the region of Piltover and the role of Midlane?',
-    answer: 'Orianna',
-  },
-  {
-    region: 'Piltover',
-    lane: 'Botlane',
-    question:
-      'Which champion is associated with the region of Piltover and the role of Botlane?',
-    answer: 'Jinx',
-  },
-  {
-    region: 'Noxus',
-    lane: 'Toplane',
-    question:
-      'Which champion is associated with the region of Noxus and the role of Toplane?',
-    answer: 'Darius',
-  },
-  {
-    region: 'Noxus',
-    lane: 'Botlane',
-    question:
-      'Which champion is associated with the region of Noxus and the role of Botlane?',
-    answer: 'Draven',
-  },
 
-  // Add more trivia questions here
-];
+
+
+
+
+"Jhin": [
+ { region: "Ionia", lane: "Botlane" }
+],
+
+  "Lux": [
+    { region: "Demacia", lane: "Midlane" },
+    { region: "Demacia", lane: "Support" }
+  ],
+  // Add more champion data here
+};
+
+// Generate trivia questions from the champion data
+const trivia = generateTriviaQuestions(championData);
+
+// Function to generate trivia questions from champion data
+function generateTriviaQuestions(championData) {
+  const trivia = [];
+  for (const [champion, regionsLanes] of Object.entries(championData)) {
+    regionsLanes.forEach(({ region, lane }) => {
+      trivia.push({
+        region,
+        lane,
+        question: `Which champion is associated with the region of ${region} and the role of ${lane}?`,
+        answer: champion,
+      });
+    });
+  }
+  return trivia;
+}
+
 let [lane1, lane2, lane3] = getRandomUniqueItems(allLanes, 3);
 let [reg1, reg2, reg3] = getRandomUniqueItems(allRegions, 3);
 // Function to check for a winner
